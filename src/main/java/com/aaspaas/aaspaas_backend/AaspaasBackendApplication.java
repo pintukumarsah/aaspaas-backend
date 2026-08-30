92211029db97e0f0289eec6ddfc9fa01d5f455dc
Pintu Kumar Sah <pintukumarsah832@gmail.com>
2026-08-30 23:30:17 +0530

    first commit

---
+++ src/main/java/com/aaspaas/aaspaas_backend/AaspaasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aaspaas.aaspaas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaspaasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaspaasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaspaas/aaspaas_backend/AaspaasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaspaas.aaspaas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaspaasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
